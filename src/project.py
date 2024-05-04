5042ef59dd0d8f13aa46382e73190007b8d4f11a
Emmitt Stidham <eman202369@gmail.com>
2024-05-04 18:29:11 -0500

    Added maze generation function and draw maze function

--- src/project.py
+++ src/project.py
@@ -18,6 +18,27 @@ class Maze:
         self.start = (1, 1)
         self.end = (ROWS - 2, COLS - 2)
 
+    def generate_maze(self):
+        cell_list = [(1, 1)]
+        while cell_list:
+            x, y = cell_list[-1]
+            self.grid[y][x] = 1
+            neighbors = [(x+2, y), (x-2, y), (x, y+2), (x, y-2)]
+            neighbors = [(nx, ny) for nx, ny in neighbors if 0 < nx < COLS-1 and 0 < ny < ROWS-1 and self.grid[ny][nx] == 0]
+            if neighbors:
+                nx, ny = random.choice(neighbors)
+                self.grid[(y + ny) // 2][(x + nx) // 2] = 1
+                cell_list.append((nx, ny))
+            else:
+                cell_list.pop()
+
+    def draw(self, screen):
+        for y in range(ROWS):
+            for x in range(COLS):
+                if self.grid[y][x] == 1:
+                    pygame.draw.rect(screen, WHITE, (x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+        pygame.draw.rect(screen, RED, (self.end[0] * CELL_SIZE, self.end[1] * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+
 class Player:
 
 def main():
