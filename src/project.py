56caa846434195c6ff47068828b6ad8328c52355
Emmitt Stidham <eman202369@gmail.com>
2024-05-04 18:25:20 -0500

    Added constants

--- src/project.py
+++ src/project.py
@@ -1,6 +1,16 @@
 import pygame
 import random
 
+WIDTH, HEIGHT = 800, 800
+CELL_SIZE = 20
+ROWS = (HEIGHT - 1) // CELL_SIZE
+COLS = (WIDTH - 1) // CELL_SIZE
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+SCORE = 10000
+
 class Maze:
 
 class Player:
